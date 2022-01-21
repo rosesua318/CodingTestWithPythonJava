a15968b69cfb0653d5534ec4ede3fe23c5dc6c8a
rosesua318 <71651038+rosesua318@users.noreply.github.com>
2022-01-21 14:40:06 +0900

    Update 7-5.py

--- Textbook/7-5.py
+++ Textbook/7-5.py
@@ -13,7 +13,7 @@ def binary_search(array, target, start, end) :
       start = mid + 1
   return None
 
-# N(가게의 부품 개수) 출력
+# N(가게의 부품 개수) 입력
 n = int(input())
 # 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
 array = list(map(int, input().split()))

b7e6712bb72a89325936393bd7fdf254a7462fbd
rosesua318 <71651038+rosesua318@users.noreply.github.com>
2022-10-01 00:02:01 +0900

    Create Q3009.java

---
+++ Baekjoon/Q3009.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] x = new int[3];
+        int[] y = new int[3];
+        int a;
+        int b;
+        
+        for(int i = 0; i < 3; i++) {
+            x[i] = sc.nextInt();
+            y[i] = sc.nextInt();
+        }
+        
+        if(x[0] == x[1]) {
+            a = x[2];
+        } else if(x[0] == x[2]) {
+            a = x[1];
+        } else {
+            a = x[0];
+        }
+        
+        if(y[0] == y[1]) {
+            b = y[2];
+        } else if(y[0] == y[2]) {
+            b = y[1];
+        } else {
+            b = y[0];
+        }
+        
+        System.out.println(a + " " + b);
+        
+        sc.close();
+    }
+}
